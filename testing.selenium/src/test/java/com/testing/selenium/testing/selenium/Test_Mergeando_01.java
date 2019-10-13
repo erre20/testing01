f65257adb9837b3a754d02fe816a18a4854cf60e
Cristian Riquelme <crrique92@gmail.com>
2019-10-13 16:01:37 -0300

    Hola ctm

---
+++ testing.selenium/src/test/java/com/testing/selenium/testing/selenium/Test_Mergeando_01.java
@@ -0,0 +1,11 @@
+package com.testing.selenium.testing.selenium;
+
+public class Test_Mergeando_01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola ctm!!");
+		
+	}
+	
+}
